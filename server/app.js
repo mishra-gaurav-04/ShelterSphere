b34c5a72101bd6cbfe3b088e4756931e80a85975
Gaurav Mishra <gaurav.info217@gmail.com>
2023-10-28 11:37:17 +0530

    added error handler

--- server/app.js
+++ server/app.js
@@ -1,10 +1,40 @@
 const express =  require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/init_db');
 const createError = require('http-errors');
 const app = express();
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGO_URI;
+
 app.use(express.json());
 app.use(cors());
+
+app.use(async(req,res,next) => {
+    next(createError.NotFound());
+})
+
+app.use(async(err,req,res,next) => {
+    res.status(err.status || 500).json({
+        status : err.status || 500,
+        message : err.message
+    })
+})
+
+const startServer = () => {
+    connectDB(MONGODB_URI)
+    .then((conn) => {
+        console.log('Database Connected')
+        app.listen(PORT,() => {
+            console.log(`Server Started :: ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+startServer();
