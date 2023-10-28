bd7733854a6a3853415222b4ae488833a07f7748
Gaurav Mishra <gaurav.info217@gmail.com>
2023-10-28 11:13:06 +0530

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import Profile from './pages/Profile'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignOut from './pages/SignOut'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='bg-slate-300 shadow-md'>
+       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to="/">
+                <h1 className='text-sm sm:text-xl flex flex-wrap'>
+                    <span className='font-bold text-gray-700'>Shelter</span>
+                    <span className='font-bold text-gray-400'>Sphere</span>
+                </h1>
+            </Link>
+            <form action="" className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-700'/>
+            </form>
+           <div className='flex gap-3'>
+                <Link className='hover:text-blue-800 hidden sm:inline' to='/'>Home</Link>
+                <Link className='hover:text-blue-800 hidden sm:inline' to='/about'>About</Link>
+                <Link className='hover:text-blue-800 hidden sm:inline' to='/sign-in'>SignIn</Link>
+           </div>
+       </div>
+    </header>
+  )
+}
+
+export default Header
