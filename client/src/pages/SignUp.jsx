1e5bf90923ac7268ecfede1c54b445cdcca53207
Gaurav Mishra <gaurav.info217@gmail.com>
2023-10-29 11:10:24 +0530

    updated signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,44 @@
 import React from 'react'
 import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
 import {signUp} from '../api/api';
 
 const SignUp = () => {
   const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleInputChange = (event) => {
       setFormData({
         ...formData,
         [event.target.id] : event.target.value
-      })
-  }
+      });
+  };
 
   const handleSubmit = async(event) => {
       event.preventDefault();
 
       try{
+        setLoading(true);
         const res = await signUp(JSON.stringify(formData));
         console.log('Response from the signup page',res);
+
+        const data = await res.json();
+        // console.log(data);
+
+        if(data.success === false){
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
+        setLoading(false);
+        setError(null);
+        navigate('/listings');
       }
       catch(err){
-        console.log(err);
+        setLoading(false);
+        setError(error.message);
       }
   }
 
@@ -32,9 +49,10 @@ const SignUp = () => {
           <input type="text" placeholder='Name' className='p-3 rounded-lg border border-gray-800' id='name' onChange={handleInputChange}/>
           <input type="text" placeholder='Email' className='p-3 rounded-lg border border-gray-800' id='email' onChange={handleInputChange}/>
           <input type="password" placeholder='Password' className='p-3 rounded-lg border border-gray-800' id='password' onChange={handleInputChange}/>
-          <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+          <button disabled={loading} type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign-Up'}</button>
         </form>
       <p className='mt-3'>Have An Account ? <Link className='text-blue-700' to='/sign-in'>Sign In</Link></p>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
