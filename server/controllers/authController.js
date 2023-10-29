835c546059c332e9614aa3e71f65ad662bdeb260
Gaurav Mishra <gaurav.info217@gmail.com>
2023-10-29 09:49:39 +0530

    added signup comtroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const createError = require('http-errors');
 const {registerSchema,loginSchema} = require('../utils/auth.validate');
 const {signAccessToken,signRefreshToken} = require('../service/token-service');
+const UserData = require('../utils/UserData');
 
 exports.signUp = async(req,res,next) => {
     try{
@@ -18,11 +19,18 @@ exports.signUp = async(req,res,next) => {
         res.cookie('accessToken',accessToken,{httpOnly:true,maxAge:60*60*1000});
         res.cookie('refreshToken',refreshToken,{httpOnly:true,maxAge:24*60*60*1000});
 
-        res.json({
-            accessToken : accessToken
+        const userData = new UserData(newUser);
+        res.status(201).json({
+            status : 'success',
+            message : 'User Created Successfully',
+            userData
         })
     }
     catch(error){
-
+        if(error.isJoi === true){
+            error.status = 422
+        }
+        next(error);
     }
 }
+
