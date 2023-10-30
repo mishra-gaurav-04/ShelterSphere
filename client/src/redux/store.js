cb34a186f10b02a9b7d1863b5be78173f1cb882b
Gaurav Mishra <gaurav.info217@gmail.com>
2023-10-30 10:27:52 +0530

    added redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
-import authReducer from './auth/auth.slice';
 
 export const store = configureStore({
-    reducer : {auth : authReducer},
-    middleware : (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck : false
-    })
-})
+    reducer : {},
+    middleware : (getDefaultMiddleware) => {
+        getDefaultMiddleware({
+            serializableCheck : false
+        })
+    }
+});
+
+
