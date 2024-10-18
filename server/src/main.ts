f2bd91821b00750d1e559ce61b96127afeca37b3
Gaurav <gaurav.info217@gmail.com>
2024-10-18 19:26:54 +0530

    added logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {LoggerFactory} from './utils/LoggerFactory';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger : LoggerFactory('ShelterSphere')
+  });
   await app.listen(3000);
 }
 bootstrap();
