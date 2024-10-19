a447424f5a814a5823473af203002ab2b5311334
Gaurav <gaurav.info217@gmail.com>
2024-10-19 18:13:08 +0530

    added auth validation schema

---
+++ server/src/auth/validation/auth-validation-schema.ts
@@ -0,0 +1,10 @@
+import * as joi from 'joi';
+
+export const registerUserSchema = joi.object({
+    name : joi.string().required(),
+    contactNumber : joi.string().required(),
+    email : joi.string().email().required(),
+    password : joi.string().required(),
+    location : joi.string().required(),
+    country : joi.string().required()
+})
